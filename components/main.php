2ea19482a8d713640bd4e96d815eb65f26105056
Nicola-Perera <nicola.perera96@mail.com>
2021-12-09 18:09:28 +0100

    add card style

--- components/main.php
+++ components/main.php
@@ -1,7 +1,9 @@
-<?php foreach ($songs as $song) : ?>   
-                <div>
-                    <div class="card">
-                        <img src="<?php echo $song['poster']; ?>" alt="song poster">
+<main>
+    <div class="cards container">
+    <?php foreach ($songs as $song) : ?>  
+            <div class="card">
+                <div class="song">
+                    <img src="<?php echo $song['poster']; ?>" alt="song poster">
                         <h2><?php echo $song['title']; ?></h2>
                         <div class="song_content">
                             <div>
@@ -9,6 +11,8 @@
                                 <h4><?php echo $song['year']; ?></h4>
                             </div>
                         </div>
-                    </div>
                 </div>
+            </div>
             <?php endforeach; ?> 
+        </div> 
+</main>
