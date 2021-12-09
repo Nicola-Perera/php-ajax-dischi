471029c34b6acf6205a78ea0f715faf4fdd8c0e7
Nicola-Perera <nicola.perera96@mail.com>
2021-12-09 15:24:01 +0100

    add header, main and footer components

--- milestone_1/index.php
+++ milestone_1/index.php
@@ -12,9 +12,22 @@ $songs = 'https://flynn.boolean.careers/exercises/api/array/music';
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>php-ajax-dischi</title>
 </head>
 <body>
+
+<!-- header -->
+    <?php require __DIR__ . '/components/head.php'; ?>
+
+<!-- main -->
+    <?php require __DIR__ . '/components/main.php'; ?>
+
+<!-- footer -->
+    <?php require __DIR__ . '/components/footer.php'; ?>
     
+<!-- axios -->
+<script src='https://cdnjs.cloudflare.com/ajax/libs/axios/0.24.0/axios.min.js' integrity='sha512-u9akINsQsAkG9xjc1cnGF4zw5TFDwkxuc9vUp5dltDWYCSmyd0meygbvgXrlc/z7/o4a19Fb5V0OUE58J7dcyw==' crossorigin='anonymous' referrerpolicy='no-referrer'></script>
+<!-- vue -->
+<script src='https://cdn.jsdelivr.net/npm/vue@2.6.14/dist/vue.js'></script>
 </body>
 </html>
