362c7fbff184f156ba1e6159718a2125df1b9106
Nicola-Perera <nicola.perera96@mail.com>
2021-12-09 13:00:48 +0100

    initial commit
    create main structure, add songs data

---
+++ milestone_1/index.php
@@ -0,0 +1,20 @@
+<!-- 
+    Stampare a schermo una decina di dischi musicali (vedi screenshot).
+    Utilizzare: Html, JS, VueJS, PHP 
+-->
+<?php
+$songs = 'https://flynn.boolean.careers/exercises/api/array/music';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
